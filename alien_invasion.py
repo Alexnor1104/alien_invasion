e3ad8db87150bc8099efc6f1066bc7cc761b05ce
Alexnor11 <advance114@gmail.com>
2021-09-04 00:58:11 +0300

    aliens first row

--- alien_invasion.py
+++ alien_invasion.py
@@ -80,7 +80,17 @@ class AlienInvasion:
     def _create_fleet(self):
         """Создание флота вторжения"""
         alien = Alien(self)
-        self.aliens.add(alien)
+        alien_width = alien.rect.width
+        available_space_x = self.settings.screen_width - (2 * alien_width)
+        number_aliens_x = available_space_x // (2 * alien_width)
+
+        # Создание первого ряда пришельцев:
+        for alien_number in range(number_aliens_x):
+            # Создание пришельца и размещение в ряду:
+            alien = Alien(self)
+            alien.x = alien_width + 2 * alien_width * alien_number
+            alien.rect.x = alien.x
+            self.aliens.add(alien)
 
     def _update_screen(self):
         """Обновляет изображения на экране и отображает новый экран"""
